a52bf6b286515321c429a44ba72d1307ca058914
KhanZuh <khanzuhair96@live.co.uk>
2024-06-04 09:06:41 +0100

    Extensions added

--- src/main/java/com/example/spring_cinema_api/controllers/MovieController.java
+++ src/main/java/com/example/spring_cinema_api/controllers/MovieController.java
@@ -34,11 +34,37 @@ public class MovieController {
         }
     }
 
-    @PostMapping
+    @PostMapping("/movies")
     public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
         Movie createdMovie = movieService.createMovie(movie);
         return new ResponseEntity<>(createdMovie, HttpStatus.CREATED);
     }
+
+    @PutMapping("/{id}")                                                                                                // Method receives movie id as path variable and the updated movie object in the request body. -
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie updatedMovie) {                  // Calls the updateMovie method from the movieService, passing the ID and updated movie.
+Movie movie = movieService.updateMovie(id, updatedMovie);                                                               // If the movie is found and successfully updated, the method returns the updated movie with an HTTP status of 200 (OK). - If the movie is not found (i.e., null is returned from the service), the method returns an HTTP status of 404 (Not Found).
+        if (movie != null) {
+            return new ResponseEntity<>(movie, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/movies/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {                                                    // The method receives the movie ID as a path variable.
+        boolean deleted = movieService.deleteMovie(id);                                                                 // It calls the deleteMovie method from the movieService, passing the ID.
+        if (deleted) {                                                                                                  // If the movie is found and successfully deleted, the method returns an HTTP status of 204 (No Content).
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);                                                             // If the movie is not found (i.e., false is returned from the service), the method returns an HTTP status of 404 (Not Found).
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+
+
+
+
 }
 
 
--- src/main/java/com/example/spring_cinema_api/services/MovieService.java
+++ src/main/java/com/example/spring_cinema_api/services/MovieService.java
@@ -24,4 +24,28 @@ public class MovieService {
     public Movie createMovie(Movie movie) {
         return movieRepository.save(movie);
     }
+
+    public Movie updateMovie(Long id, Movie updatedMovie) {                   // Using movieRepository.findById(id).orElse(null) to retrieve the movie by its ID. If the movie is not found, returns null. If the movie is found, update its properties and saves the changes to the database using movieRepository.save(movie) --> return the updated movie.
+        Movie movie = movieRepository.findById(id).orElse(null);
+        if (movie != null) {
+            movie.setTitle(updatedMovie.getTitle());
+            movie.setRating(updatedMovie.getRating());
+            movie.setDuration(updatedMovie.getDuration());
+            return movieRepository.save(movie);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean deleteMovie(Long id) {                                               //  Using movieRepository.findById(id).orElse(null) to retrieve the movie by its ID. If the movie is not found, return false. If the movie is found, delete it from the database using movieRepository.delete(movie) and return true.
+        Movie movie = movieRepository.findById(id).orElse(null);
+        if (movie != null) {
+            movieRepository.delete(movie);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
